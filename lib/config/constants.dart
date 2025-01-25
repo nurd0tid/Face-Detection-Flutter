04680331bd7e1b4c54dd23b302ee5558da3faf5c
nurd0tid <mnur29054@gmail.com>
2025-01-25 23:53:29 +0700

    update treshold

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,4 @@
 const String SUPABASE_URL = ''; // Ganti dengan URL proyek Supabase Anda
 const String SUPABASE_KEY = ''; // Ganti dengan Anon Key Supabase Anda
 const String DEFAULT_USER_ID = '123e4567-e89b-12d3-a456-426614174000';
-const double FACE_SIMILARITY_THRESHOLD = 0.75; // Ambang batas kemiripan wajah
+const double FACE_SIMILARITY_THRESHOLD = 0.8; // Ambang batas kemiripan wajah
